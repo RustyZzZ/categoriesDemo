f1cc564f0f2bca0e668d9be66ec8112773e908ed
Rostyslav Diachuk <Rostyslav_Diachuk@epam.com>
2020-12-15 14:02:29 +0200

    Initial project

---
+++ categoriesDemo/src/main/java/com/example/categoriesDemo/CategoriesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.categoriesDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoriesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategoriesDemoApplication.class, args);
+	}
+
+}
---
+++ categoriesDemo/src/test/java/com/example/categoriesDemo/CategoriesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.categoriesDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoriesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
